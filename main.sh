94099c7cd43a82372486f3a46eb99f44347bdfef
gabriel <batistutag190@gmail.com>
2024-08-08 11:03:54 -0300

    remove others distro suport

--- main.sh
+++ main.sh
@@ -1,18 +1,6 @@
 if ! command -v wget &> /dev/null; then
     echo "wget não encontrado. Instalando..."
-    # Identifica a distribuição e instala o wget
-    if [ -x "$(command -v apt)" ]; then
-        sudo apt update && sudo apt install -y wget
-    elif [ -x "$(command -v dnf)" ]; then
-        sudo dnf install -y wget
-    elif [ -x "$(command -v yum)" ]; then
-        sudo yum install -y wget
-    elif [ -x "$(command -v pacman)" ]; then
-        sudo pacman -S --noconfirm wget
-    else
-        echo "Gerenciador de pacotes não suportado. Instale wget manualmente."
-        exit 1
-    fi
+    sudo apt update && sudo apt install -y wget
 fi
 
 wget https://archive.org/download/authy/authy.deb
